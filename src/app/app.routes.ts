5be540426a8afb1b9eb7a4a9260f1e5a3d2154f5
Sandip <srai68141@gmail.com>
2025-01-02 21:25:52 +0545

    feat: added routes for navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {TablesComponent} from './components/tables/tables.component';
+import {ContactComponent} from './components/contact/contact.component';
+import {NewsComponent} from './components/news/news.component';
+import {AboutComponent} from './components/about/about.component';
+import {PasgenotfoundComponent} from './components/pasgenotfound/pasgenotfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'home', component: TablesComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'news', component: NewsComponent},
+  {path: 'aboutus', component: AboutComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: PasgenotfoundComponent}
+];
